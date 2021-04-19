f47f156c92d9e39ffd4008fa2c9483c230432eef
Eugene <test@test.com>
2021-04-19 14:55:10 -0700

    mission dics parser

--- python/list.py
+++ python/list.py
@@ -34,9 +34,12 @@ def get_version(name):
 # ------------------------------------------------------------
 
 class ListSchemas(HTMLParser):
-  def __init__(self):
+  def __init__(self, base_url, ns_index):
     super().__init__()
     self.last_ns = ""
+    self.base_url = base_url
+    self.ns_index = ns_index
+
 
   def handle_starttag(self, tag, attrs):
     if tag != "a": 
@@ -45,7 +48,7 @@ class ListSchemas(HTMLParser):
     href = get_href(attrs)
     if href and href.lower().endswith(".json"):
       tokens = href.split('/')
-      ns = tokens[2]
+      ns = tokens[self.ns_index]
 
       # Only process first entry
       if self.last_ns == ns: 
@@ -53,16 +56,35 @@ class ListSchemas(HTMLParser):
 
       self.last_ns = ns
       name = tokens[-1]
-      print(ns + "," + href + "," + get_version(name))
+      print('"' + ns + '","' + self.base_url + href + '","' + get_version(name) + '"')
 
 
 # ------------------------------------------------------------
 
-f = open("c:\\tmp\\schema\\index-1.15.0.0.shtml", "r", encoding="utf-8")
-data = f.read()
-f.close()
+def parse_pds_dics():
+# https://pds.nasa.gov/datastandards/dictionaries/index-1.15.0.0.shtml
+  ns_index = 2
+  f = open("c:\\tmp\\schema\\index-1.15.0.0.shtml", "r", encoding="utf-8")
+  data = f.read()
+  f.close()
 
-parser = ListSchemas()
-parser.feed(data)
-parser.close()
+  parser = ListSchemas("https://pds.nasa.gov", ns_index)
+  parser.feed(data)
+  parser.close()
 
+# ------------------------------------------------------------
+
+def parse_missions_dics():
+# https://pds.nasa.gov/datastandards/dictionaries/index-missions.shtml
+  ns_index = 3
+  f = open("c:\\tmp\\schema\\Missions.shtml", "r", encoding="utf-8")
+  data = f.read()
+  f.close()
+
+  parser = ListSchemas("https://pds.nasa.gov", ns_index)
+  parser.feed(data)
+  parser.close()
+
+# ------------------------------------------------------------
+
+parse_missions_dics()
